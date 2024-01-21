b0fc97ac205617085400d71213c562d5ef725e95
Zeliq <zeliqzayyan@gmail.com>
2024-01-21 11:49:09 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from '../images/box.jpg';
+import image from "../images/box.jpg";
 
 const imageAltText = "A contrast white light box in a black background.";
 
